68b3337541381b092cfa94feb07747a1d26d4987
vravish <vravish@us.ibm.com>
2016-06-22 18:42:11 -0400

    Adding working Maven code with a slight change

---
+++ src/main/java/com/ibm/whc/TomcatWebAppForChefNode/AdditionServlet.java
@@ -0,0 +1,27 @@
+package com.ibm.whc.TomcatWebAppForChefNode;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SimpleServlet
+ */
+public class AdditionServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	int a = Integer.parseInt(request.getParameter("a"));
+    	int b = Integer.parseInt(request.getParameter("b"));
+    	int sum = a+b;
+    	response.getWriter().println("06-22-16 18:40 PM: The sum of the two numbers is " + sum + ".");
+    }
+
+}
