d611a0280f8cc1897bb162a7c3d907873b640278
vravish <vravish@us.ibm.com>
2016-06-30 21:43:50 -0400

    Using sum % 2 as the exit status

--- src/main/java/com/ibm/whc/TomcatWebAppForChefNode/AdditionServlet.java
+++ src/main/java/com/ibm/whc/TomcatWebAppForChefNode/AdditionServlet.java
@@ -21,10 +21,9 @@ public class AdditionServlet extends HttpServlet {
     	int a = Integer.parseInt(request.getParameter("a"));
     	int b = Integer.parseInt(request.getParameter("b"));
     	int sum = a+b;
-    	int random = (int) (Math.random()*2);
     	response.getWriter().println("06-30-16 21:06: The sum of the two numbers is " + sum + ".");
-    	response.getWriter().println("The exit status will be: " + random);
-    	response.addIntHeader("exitstatus", random);
+    	response.getWriter().println("The exit status will be: " + sum % 2);
+    	response.addIntHeader("exitstatus", sum % 2);
     }
 
 }
