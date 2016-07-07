f15836b45f5cc7bb09031c3c369193faec99376b
vravish <vravish@us.ibm.com>
2016-07-07 12:24:11 -0400

    Added swagger.json and checking working directory in webapp

---
+++ src/main/java/com/ibm/whc/TomcatWebAppForChefNode/XMLTestServlet.java
@@ -0,0 +1,34 @@
+package com.ibm.whc.TomcatWebAppForChefNode;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class XMLTestServlet
+ */
+public class XMLTestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().println("Working directory: " + new File(".").getCanonicalPath());
+		response.getWriter().println(new ArrayList<String>(Arrays.asList(new File(".").list())));
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
