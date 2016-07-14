49ddead247f680f9e964fc0c76c24ac0169c8ae5
vravish <vravish@us.ibm.com>
2016-07-14 11:33:01 -0400

    Catting the output files

--- test.sh
+++ test.sh
@@ -2,6 +2,13 @@ mkdir tests
 
 wget --server-response --output-document=tests/TEST-swagger.xml --header="Content-Type: application/json" --post-data='{"swaggerURL":"http://169.44.118.61:8080/TomcatWebAppForChefNode/swagger.json"}' http://169.44.116.214:9080/SwaggerParser/api/swaggerTestService &> wget.out
 
+echo Catting wget.out:
+cat wget.out
+echo File ends here.
+
+echo Catting tests/TEST-swagger.xml:
+cat tests/TEST-swagger.xml
+echo File ends here.
 
 MY_EXIT_CODE=$(grep -m1 teststatus wget.out | cut -d':' -f2)
 echo My exit code is $MY_EXIT_CODE
