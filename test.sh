56fc7105c7940f14ca7e555a16e069366009ff05
vravish <vravish@us.ibm.com>
2016-07-14 11:19:33 -0400

    Changed test.sh to use stderr and stdout to find headers

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 mkdir tests
 
-wget --server-response --output-document=tests/TEST-swagger.xml --header="Content-Type: application/json" --post-data='{"swaggerURL":"http://169.44.118.61:8080/TomcatWebAppForChefNode/swagger.json"}' http://169.44.116.214:9080/SwaggerParser/api/swaggerTestService > wget.out
+wget --server-response --output-document=tests/TEST-swagger.xml --header="Content-Type: application/json" --post-data='{"swaggerURL":"http://169.44.118.61:8080/TomcatWebAppForChefNode/swagger.json"}' http://169.44.116.214:9080/SwaggerParser/api/swaggerTestService &> wget.out
 
 
 MY_EXIT_CODE=$(grep -m1 teststatus wget.out | cut -d':' -f2)
