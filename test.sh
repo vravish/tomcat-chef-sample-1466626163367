766af6d8f7576be02a73b919aa3d41324650ce43
vravish <vravish@us.ibm.com>
2016-07-14 11:36:43 -0400

    Sleeping after Wget

--- test.sh
+++ test.sh
@@ -2,6 +2,8 @@ mkdir tests
 
 wget --server-response --output-document=tests/TEST-swagger.xml --header="Content-Type: application/json" --post-data='{"swaggerURL":"http://169.44.118.61:8080/TomcatWebAppForChefNode/swagger.json"}' http://169.44.116.214:9080/SwaggerParser/api/swaggerTestService &> wget.out
 
+sleep 10
+
 echo Catting wget.out:
 cat wget.out
 echo File ends here.
