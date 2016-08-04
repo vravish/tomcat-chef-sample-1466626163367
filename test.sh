54e3cdfb35e38dae646b4e26bbf64a1eae355a1d
vravish <vravish@us.ibm.com>
2016-08-04 17:10:04 -0400

    Updated IP address in test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 mkdir tests
 
-curl -i -H "Content-Type: application/json" -X POST -d '{"swaggerURL":"http://169.44.118.61:8080/TomcatWebAppForChefNode/swagger.json"}' http://169.44.116.214:9080/SwaggerParser/api/swaggerTestService | sed 's/\r//' > curl.out
+curl -i -H "Content-Type: application/json" -X POST -d '{"swaggerURL":"http://169.44.127.142:8080/TomcatWebAppForChefNode/swagger.json"}' http://169.44.116.214:9080/SwaggerParser/api/swaggerTestService | sed 's/\r//' > curl.out
 printf "\n" >> curl.out
 
 print_real_lines() {
